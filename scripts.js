790a87f275a7c1e8c39b0fe46a20de36a14c0f3f
ooepi <robert.harmaakivi@gmail.com>
2020-12-03 18:08:20 +0200

    finished frontpage with sidebar and product cards

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function slideshow() {
+    var i;
+    var x = document.getElementsByClassName("slides");
+    for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+    }
+    index++;
+    if (index > x.length) {index = 1}    
+    x[index-1].style.display = "block";  
+    setTimeout(slideshow, 8000);
+}
+
+function addtobasket() {
+    alert("Tuote lisättiin ostoskoriin!");
+}
